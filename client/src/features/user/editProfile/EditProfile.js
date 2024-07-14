bfe203e019778450920e75832c793f88dd753d67
ABERsara <sara0556799675@gmail.com>
2024-07-14 14:27:49 +0300

    changes on users desplay

--- client/src/features/user/editProfile/EditProfile.js
+++ client/src/features/user/editProfile/EditProfile.js
@@ -2,7 +2,18 @@ import "./edit-Profile.css"
 
 const EditProfile = () => {
   return (
-    <div>EditProfile</div>
+    <div className="edit-profile">
+      <form className="edit-profile-form">
+      <input type="file" name="userImage" placeholder="העלה תמונה" />
+        <input type="text" name="firstName" required placeholder="הכנס שם פרטי" />
+        <input type="text" name="lastName" placeholder="הכנס שם משפחה" />
+        <input type="phone" name="phone" placeholder="הכנס מס' נייד" />
+        <input type="email" name="email" required placeholder="הכנס כתובת אימייל" />
+        <input type="password" name="password" required placeholder="הכנס סיסמה" />
+        <button type="submit">עדכן</button>
+        <button type="cancel">בטל</button>
+      </form>
+    </div>
   )
 }
 
