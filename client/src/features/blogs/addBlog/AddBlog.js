b466a2c944a2a27269f5c70fb2d9d6b3065a631b
ABERsara <sara0556799675@gmail.com>
2024-08-20 13:04:15 +0300

    Add apiBlogSlice and update addBlog

--- client/src/features/blogs/addBlog/AddBlog.js
+++ client/src/features/blogs/addBlog/AddBlog.js
@@ -1,8 +1,33 @@
 import "./add-blog.css"
 
+import {useAddBlogMutation} from "../blogsApiSlice"
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
 const AddBlog = () => {
+  const [addBlog, {data, isError, error, isSuccess, isLoading}] = useAddBlogMutation()
+  const navigate = useNavigate()
+  useEffect(()=>{
+    if(isSuccess){
+      navigate("/dash/blogs")
+    }
+
+  }, [isSuccess])
+  const formSubmit = (e) =>{
+    e.preventDefault()
+      const data = new FormData(e.target)
+      const blogObject =Object.fromEntries(data.entries())
+      addBlog(blogObject)
+  }
   return (
-    <div>addBlog</div>
+    <div className="add-blog-container">
+        <form onSubmit={formSubmit} className="add-blog-form">
+            <input type="text" required name="title" placeholder="כותרת" />
+            <input type="text" required name="content" placeholder="תוכן" />
+            <input type="file"  name="blog"/>
+            <button  type="submit">שלח</button>
+        </form>
+
+    </div>
   )
 }
 
---
+++ client/src/features/blogs/blogsApiSlice.js
@@ -0,0 +1,45 @@
+import apiSlice from "../../app/apiSlice";
+
+const blogsApiSlice = apiSlice.injectEndpoints({
+    endpoints: (build) =>({
+        getAllBlogs:build.query({
+            query:()=>({
+                url: "/api/blogs"
+            }),
+            providesTags:["Blogs"]
+
+        }),
+        addBlog:build.mutation({
+            query:(blog) =>({
+                url: "/api/blogs",
+                method: "POST",
+                body: blog
+            }),
+            invalidatesTags: ["Blogs"]
+        }),
+        updateBlog:build.mutation({
+            query:(blog) =>({
+                url: "/api/blogs",
+                method: "PUT",
+                body: blog
+            }),
+            invalidatesTags: ["Blogs"]
+        }),
+        deleteBlog:build.mutation({
+            query:({_id}) =>({
+                url: "/api/blogs",
+                method: "Delete",
+                body: {_id}
+            }),
+            invalidatesTags: ["Blogs"]
+        })
+    })
+    
+})
+
+export const { useGetAllBlogsQuery,
+     useAddBlogMutation ,
+      useUpdateBlogMutation,
+      useDeleteBlogMutation
+
+    }  = blogsApiSlice
